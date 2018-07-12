3014511c1e2caae43953004ffba84f7802f6daf1
José Manuel Martínez Blancas <manolo@itia.mx>
2018-07-12 18:15:49 -0500

    Students, Courses & Enrollments

---
+++ WebApplication2/Models/Catalogo.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+
+namespace WebApplication2.Models
+{
+    public class Catalog
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+    }
+
+    public class CatalogDBContext : DbContext
+    {
+        public DbSet<Catalog> Catalogs { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
